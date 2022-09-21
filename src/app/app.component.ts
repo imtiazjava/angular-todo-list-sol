d3dfd28001ec653a94708297eaaa70d21e116ca6
Imtiaz <imtiazjavatrainer@gmail.com>
2022-09-21 15:59:24 +0530

    'todo-list'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   styleUrls: ['./app.component.css'],
+  
+
 })
-export class AppComponent {
-  title = 'myapp';
+ export class AppComponent {
+   title:string='TO-DO-TASKS';
+   listTask:any[]=[];
+   addTask(task:any):void{
+     this.listTask.push({tid:this.listTask.length,name:task});
+     console.log(this.listTask)
+   }
+
+   deleteItem(id:any):void{
+     alert('id:'+id);
+     if(id==1){
+        this.listTask.pop();
+     }else{
+      this.listTask.splice(id,1);
+     }
+     //this.listTask.splice(id,2);
+     // this.listTask.pop()
+     console.log(this.listTask)
+   }
 }
